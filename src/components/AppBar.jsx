62b5f620ac99560528656911d6cd9dc9c0e710c7
mostafaIn <mostafa2015se@gmail.com>
2020-12-05 05:19:57 +0200

    Exercise 10.7: scrollable app bar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,24 +1,62 @@
 import React from 'react';
 import { Link } from 'react-router-native';
-import { StyleSheet, View, TouchableWithoutFeedback } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import Constants from 'expo-constants';
 import AppBarTab from './AppBarTab';
 import theme from '../theme';
 
 const AppBar = () => {
   return (
-    <View style={styles.container}>
-      <Link to="/">
-      <TouchableWithoutFeedback>
-        <AppBarTab tabName="Repositories" />
-      </TouchableWithoutFeedback>
-      </Link>
-      <Link to="/signin">
-       <TouchableWithoutFeedback>
-        <AppBarTab tabName="Sign in" />
-       </TouchableWithoutFeedback>
-      </Link>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView horizontal style={styles.scrollView}>
+        <Link to="/">
+          <TouchableWithoutFeedback>
+            <AppBarTab tabName="Repositories" />
+          </TouchableWithoutFeedback>
+        </Link>
+        <Link to="/signin">
+          <TouchableWithoutFeedback>
+            <AppBarTab tabName="Sign in" />
+          </TouchableWithoutFeedback>
+        </Link>
+        <Link to="/">
+          <TouchableWithoutFeedback>
+            <AppBarTab tabName="Repositories" />
+          </TouchableWithoutFeedback>
+        </Link>
+        <Link to="/signin">
+          <TouchableWithoutFeedback>
+            <AppBarTab tabName="Sign in" />
+          </TouchableWithoutFeedback>
+        </Link>
+        <Link to="/">
+          <TouchableWithoutFeedback>
+            <AppBarTab tabName="Repositories" />
+          </TouchableWithoutFeedback>
+        </Link>
+        <Link to="/signin">
+          <TouchableWithoutFeedback>
+            <AppBarTab tabName="Sign in" />
+          </TouchableWithoutFeedback>
+        </Link>
+        <Link to="/">
+          <TouchableWithoutFeedback>
+            <AppBarTab tabName="Repositories" />
+          </TouchableWithoutFeedback>
+        </Link>
+        <Link to="/signin">
+          <TouchableWithoutFeedback>
+            <AppBarTab tabName="Sign in" />
+          </TouchableWithoutFeedback>
+        </Link>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
@@ -28,8 +66,11 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight + 20,
     backgroundColor: theme.colors.backgroundSecondary,
-    display:'flex',
-    flexDirection:'row'
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  scrollView: {
+    marginHorizontal: 10,
   },
   // ...
 });
